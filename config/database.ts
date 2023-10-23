b1c2b0adb115d31976770fedadae5835cecd19b8
Pablo Rizo <prozasoconw4@gmail.com>
2023-10-23 11:33:17 -0600

    FEAT: A PROD

--- config/database.ts
+++ config/database.ts
@@ -2,11 +2,11 @@ export default ({ env }) => ({
   connection: {
     client: "mysql",
     connection: {
-      host: env("DATABASE_HOST", "localhost"),
+      host: env("DATABASE_HOST", "138.197.209.230"),
       port: env.int("DATABASE_PORT", 3306),
-      database: env("DATABASE_NAME", "cosbiome_crm"),
-      user: env("DATABASE_USERNAME", "cosbiome"),
-      password: env("DATABASE_PASSWORD", "Ac03901582"),
+      database: env("DATABASE_NAME", "crm"),
+      user: env("DATABASE_USERNAME", "penguin"),
+      password: env("DATABASE_PASSWORD", "Ac03901582."),
       ssl: env.bool("DATABASE_SSL", false),
       charset: env("DATABASE_CHARSET", "utf8mb4"),
     },
