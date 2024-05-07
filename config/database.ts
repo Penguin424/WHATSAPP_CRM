7c51f3c151160e746e14f867363bd7c7fc183ada
Pablo Rizo <prozasoconw4@gmail.com>
2024-05-07 10:37:24 -0600

    FIX: KEYS DE PRODUCCION EN PUBLICO HUB

--- config/database.ts
+++ config/database.ts
@@ -2,11 +2,11 @@ export default ({ env }) => ({
   connection: {
     client: "mysql",
     connection: {
-      host: env("DATABASE_HOST", "138.197.209.230"),
+      host: env("DATABASE_HOST", "localhost"),
       port: env.int("DATABASE_PORT", 3306),
       database: env("DATABASE_NAME", "crm"),
-      user: env("DATABASE_USERNAME", "penguin"),
-      password: env("DATABASE_PASSWORD", "Ac03901582."),
+      user: env("DATABASE_USERNAME", "root"),
+      password: env("DATABASE_PASSWORD", "123456"),
       ssl: env.bool("DATABASE_SSL", false),
       charset: env("DATABASE_CHARSET", "utf8mb4"),
     },
